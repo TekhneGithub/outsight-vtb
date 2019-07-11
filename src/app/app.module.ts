243b1e153d7c2d43ef25b6f78e0be077c926cabd
Kapil Gorve <kapilsg186@gmail.com>
2019-07-12 03:53:30 +0530

    add multipage routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,6 @@ import { NavigationComponent } from "./component/navigation/navigation.component
 import { HomeComponent } from "./pages/home/home.component";
 import { OverviewComponent } from "./component/home/overview/overview.component";
 import { ExtraInfoComponent } from "./component/home/extra-info/extra-info.component";
-import { MainModule } from "./pages/main/main.module";
 import { MainComponent } from "./pages/main/main.component";
 import { FlightsComponent } from './component/home/flights/flights.component';
 import { TripDaysComponent } from './component/home/trip-days/trip-days.component';
@@ -52,6 +51,14 @@ import { OutsightTravelComponent } from './component/home/outsight-travel/outsig
         resolve: {
           itinerary: ItineraryResolver
         }
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: 'main',
+        component: MainComponent,
       }
     ]),
     AgmCoreModule.forRoot({
