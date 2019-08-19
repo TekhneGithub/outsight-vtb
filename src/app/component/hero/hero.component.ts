0b0de7587ecbac41ce2c4ba998968fdbbf95ba83
anand <anand@tekhne.nl>
2019-08-19 12:03:45 +0530

    add the traverl namein banner

--- src/app/component/hero/hero.component.ts
+++ src/app/component/hero/hero.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { MappingService } from 'src/app/services/mapping.service';
 
 @Component({
   selector: "app-hero",
@@ -8,14 +9,17 @@ import { Component, OnInit, Input } from "@angular/core";
 export class HeroComponent implements OnInit {
   @Input() itinerary: any;
 
+  constructor(private mappingService: MappingService) {
+  }
   ngOnInit() {
-    console.log("Hero" + this.itinerary);
+    //console.log("Hero" + this.itinerary.participants['party 1'][0].name);
    this.itinerary.title = this.itinerary.participants['party 1'][0].name + ' ' + this.itinerary.participants['party 1'][0].surname + ' & ' + this.itinerary.participants['party 1'][1].name + ' ' + this.itinerary.participants['party 1'][1].surname;
 
+   console.log("hero Name " + this.itinerary.title)
 
 
 
-    // this.itinerary.subTitle = this.mappingService.countryByCode(this.itinerary.extraFieldValues[0].fields[0].selected);
+    this.itinerary.subTitle = this.mappingService.countryByCode(this.itinerary.extraFieldValues[0].fields[0].selected);
 
 
   }
