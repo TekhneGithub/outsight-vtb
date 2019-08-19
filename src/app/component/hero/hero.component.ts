7b8100681fddeb2967e970b5ddfebcf7810ab23a
Yogesh <yogesh@tekhne.nl>
2019-08-19 11:46:41 +0530

    bestemming1 and bestemming2 mapping

--- src/app/component/hero/hero.component.ts
+++ src/app/component/hero/hero.component.ts
@@ -7,13 +7,22 @@ import { Component, OnInit, Input } from "@angular/core";
 })
 export class HeroComponent implements OnInit {
   @Input() itinerary: any;
+  heroTitle = '';
 
   ngOnInit() {
     console.log("Hero" + this.itinerary);
    this.itinerary.title = this.itinerary.participants['party 1'][0].name + ' ' + this.itinerary.participants['party 1'][0].surname + ' & ' + this.itinerary.participants['party 1'][1].name + ' ' + this.itinerary.participants['party 1'][1].surname;
 
-
-
+    for(const extraFieldValue of this.itinerary.extraFieldValues) {
+      for (const field of extraFieldValue.fields) {
+        if (field.name == 'bestemming1' || field.name == 'bestemming2') {
+          if(field.value) {
+            this.heroTitle += ' ' + field.value;
+            this.heroTitle = this.heroTitle.trim();
+          }
+        }
+      }
+    }
 
     // this.itinerary.subTitle = this.mappingService.countryByCode(this.itinerary.extraFieldValues[0].fields[0].selected);
 
