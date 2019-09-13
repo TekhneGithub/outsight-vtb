86ac8caea42acce5b1c20786426f56102eaf67b1
Yogesh <yogesh@tekhne.nl>
2019-09-13 19:17:54 +0530

    trip binded with media

--- src/app/component/home/trip-days/trip-days.component.ts
+++ src/app/component/home/trip-days/trip-days.component.ts
@@ -15,7 +15,14 @@ export class TripDaysComponent implements OnInit {
   ngOnInit() {
     for(const segment of this.itinerary.segments) {
       //map media to url take first one if applicable fix later
-      var obj = { startDay: segment.day, endDay: segment.day + segment.nights, title: segment.title + segment.subTitle, content: segment.content, media: "https://via.placeholder.com/300"};
+      var mediaURL = '';
+      if(segment.media[0] !== undefined) {
+        mediaURL = segment.media[0].url;
+      } else {
+        mediaURL = 'https://via.placeholder.com/300';
+      }
+
+      var obj = { startDay: segment.day, endDay: segment.day + segment.nights, title: segment.title + segment.subTitle, content: segment.content, media:mediaURL};
       this.titles.push(obj);
 
       //this.titles.push({ "date": "" , "title" :"" });
