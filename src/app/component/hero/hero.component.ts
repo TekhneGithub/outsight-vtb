2555fc307d17a56663b05f53f60abe79479a9573
Yogesh <yogesh@tekhne.nl>
2019-09-13 19:06:06 +0530

    cover binded

--- src/app/component/hero/hero.component.ts
+++ src/app/component/hero/hero.component.ts
@@ -9,10 +9,18 @@ import { MappingService } from 'src/app/services/mapping.service';
 export class HeroComponent implements OnInit {
   @Input() itinerary: any;
   heroTitle = '';
+  title = '';
+  subTitle = '';
+  coverImage = '';
 
   constructor(private mappingService: MappingService) {
   }
   ngOnInit() {
+
+    this.title = this.itinerary.title;
+    this.subTitle = this.itinerary.subTitle;
+    this.coverImage = this.itinerary.cover[0].url;
+
     for(const extraFieldValue of this.itinerary.extraFieldValues) {
       for (const field of extraFieldValue.fields) {
         if (field.name == 'bestemming1' || field.name == 'bestemming2') {
