d1ac6265c91aa9461667da294c21e805c4b171d4
Yogesh <yogesh@tekhne.nl>
2019-12-10 18:02:05 +0530

    Removed unitId 5 for hotels

--- vtb.js
+++ vtb.js
@@ -31,7 +31,7 @@ const customTransforms = {
     var hotels =[];
     for(const seg of obj.dst.segments) {
       for(const el of seg.elements) {
-        if(el.unitId == 5 || el.unitId == 2) {
+        if(el.unitId == 2) {
           let image = el.media[0] !== undefined ? el.media[0].url : undefined;
           var data = {title: el.title, additionalTaxt: el.additionalText, image: image};
           hotels.push(data);
